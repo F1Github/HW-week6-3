bd5a76474370475fe042e571789e7f1f024d5864
Napat Moonpinij <f1.2544pat@gmail.com>
2020-09-15 14:39:36 +0700

    Add project files.

---
+++ HW-week6 3/Source.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int N;
+	scanf_s("%d", &N);
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			printf("* ");
+		}
+		if (i != N)
+		{
+			printf("\n");
+		}
+	}
+	return 0;
+}
